a69364e64adffd6ce0aea21e156724f010fe3bb2
a-bhi-r <57566100+a-bhi-r@users.noreply.github.com>
2021-04-08 21:37:49 +0530

    added class skeleton version1

---
+++ index.py
@@ -0,0 +1,81 @@
+import sqlite3
+
+
+class Patient:
+    def __init__(self):
+        ...
+
+    def register(self):
+        ...
+
+    def login(self):
+        ...
+
+    def search(self):
+        ...
+
+    def book_appointments(self):
+        ...
+
+    def confirmation(self):
+        ...
+
+
+class Doctor:
+    def __init__(self):
+        ...
+
+    def register(self):
+        ...
+
+    def fill_info(self):
+        ...
+
+    def patient_details(self):
+        ...
+
+    def patient_report(self):
+        ...
+
+
+class Admin:
+    def __init__(self):
+        ...
+
+    def add_hospital(self):
+        ...
+
+    def add_doctor(self):
+        ...
+
+    def maintain_doctor_details(self):
+        ...
+
+    def maintain_patient_details(self):
+        ...
+
+    def cancel_doctors(self):
+        ...
+
+
+class Hospital:
+    def __init__(self):
+        ...
+
+    def add_doctor(self):
+        ...
+
+    def add_fee(self):
+        ...
+
+    def view_patients(self):
+        ...
+
+    def notify_patients(self):
+        ...
+
+    def maintain_appointment_schedule(self):
+        ...
+
+    def maintain_details(self):
+        ...
